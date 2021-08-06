2eaa16a480c4bec5381ed322338dedff6ae5278a
Valentín <valentinc1515@gmail.com>
2021-08-06 19:46:06 -0300

    no supe arreglar los warnings, hora de preguntarle a heber tonight

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import {Header} from "./componentes/Header";
-import {Start} from "./componentes/Start";
-import {Game} from "./componentes/Game";
-import {Gallery} from "./componentes/Gallery";
-import {Footer} from "./componentes/Footer";
-import {Trailer} from "./componentes/Trailer";
+import Header from "./componentes/Header";
+import Start from "./componentes/Start";
+import Game from "./componentes/Game";
+import Gallery from "./componentes/Gallery";
+import Footer from "./componentes/Footer";
+import Trailer from "./componentes/Trailer";
 
 function App() {
   return (
