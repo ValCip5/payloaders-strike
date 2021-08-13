6fc3b2d31bc463621da01c80d75cfc689b278b98
Valentín <valentinc1515@gmail.com>
2021-08-13 19:09:16 -0300

    nav completamente responsive

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -9,15 +9,15 @@ function Header() {
         <header>
                 <Navbar fixed="top" collapseOnSelect expand="lg">
                   <Container className="responsiveNav">
-                  <Navbar.Brand href="#home">Payloaders Strike!</Navbar.Brand>
+                  <Navbar.Brand href="#home"><a href='#'><img src='https://svgshare.com/i/_BD.svg' title='payicono' /></a></Navbar.Brand>
                   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                   <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                      <Nav.Link href="#features">Start</Nav.Link>
-                      <Nav.Link href="#pricing">Game</Nav.Link>
-                      <Nav.Link href="#pricing">Gallery</Nav.Link>
-                      <Nav.Link href="#pricing">Trailer</Nav.Link>
-                      <Nav.Link className="lineaAbajo" href="#pricing">Play</Nav.Link>
+                    <Nav className="ms-auto">
+                      <Nav.Link href="#start">Start</Nav.Link>
+                      <Nav.Link href="#game">Game</Nav.Link>
+                      <Nav.Link href="#gallery">Gallery</Nav.Link>
+                      <Nav.Link href="#trailer">Trailer</Nav.Link>
+                      <Nav.Link className="lineaAbajo" href="#play">Play</Nav.Link>
                     </Nav>
                   </Navbar.Collapse>
                   </Container>
