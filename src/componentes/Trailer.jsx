de62654bea9211924055c621e74c0b844279ffee
Valentín <valentinc1515@gmail.com>
2021-08-13 19:26:14 -0300

    emparejo los botones en responsive

--- src/componentes/Trailer.jsx
+++ src/componentes/Trailer.jsx
@@ -13,7 +13,7 @@ function Trailer() {
 
           
           <h2 className="col-sm-12 py-2 px-5 text-center elBotonDos">
-              <a className="py-3 px-5" id="play" href="https://all-and-the-gang.itch.io/payloaders-strike"><i className="bi-play-fill pe-3"></i>PLAY THE DEMO</a>
+              <a className="py-3 px-3" id="play" href="https://all-and-the-gang.itch.io/payloaders-strike"><i className="bi-play-fill pe-3"></i>PLAY THE DEMO</a>
           </h2>
     </section>
   )
