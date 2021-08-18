4e2800a3867fcb57305cc30f6930f55d2d7e4dd5
LiamAcostaLipera <liamnet2000@gmail.com>
2021-08-18 00:41:17 -0300

    video youtube

--- src/componentes/Trailer.jsx
+++ src/componentes/Trailer.jsx
@@ -9,9 +9,8 @@ function Trailer() {
         <h2 className="tituloUno">
           TRAILER
         </h2>
+        <iframe className="py-4" src="https://www.youtube.com/embed/Jt5vMFyMeWA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
-        <iframe className="py-4" src="https://www.youtube-nocookie.com/embed/gj32vekphz8?start=170" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture">
-        </iframe>
         <h2 className="col-sm-12 py-4 text-center elBotonDos">
           <a className="py-3 px-3" id="play" href="https://drive.google.com/uc?export=download&id=1QxY09ee5keD2--4w70JMsb7tv5PlY5YX" target="_blank" rel="noreferrer"><i className="bi-play-fill pe-3"></i>PLAY THE DEMO</a>
         </h2>
